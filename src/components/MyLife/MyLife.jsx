bc22300bb2b52bde9582a0cb5252670a8fa2087e
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-05-23 19:44:16 +0100

    coherence in all pages

--- src/components/MyLife/MyLife.jsx
+++ src/components/MyLife/MyLife.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import "./MyLife.css";
 
 const MyLife = () => {
-  return <div class="about-my-life">MyLife</div>;
+  return (
+    <section class="about-my-life">
+      <h2>My Life</h2>
+      <div class="main-about-my-life">
+        <p>olaa</p>
+      </div>
+    </section>
+  );
 };
-
 export default MyLife;
--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -3,7 +3,14 @@ import React from "react";
 import "./MyWork.css";
 
 const MyWork = () => {
-  return <div class="about-my-work">MyWork</div>;
+  return (
+    <section class="about-my-work">
+      <h2>My Work</h2>
+      <div class="main-about-my-work">
+        <p>olaa</p>
+      </div>
+    </section>
+  );
 };
 
 export default MyWork;
