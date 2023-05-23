6d2754921d46ba962ee8f75a5f696b2217bec5bf
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-05-23 17:17:41 +0100

    add countstuff bar

--- src/components/CountStuff/CountStuff.jsx
+++ src/components/CountStuff/CountStuff.jsx
@@ -5,18 +5,10 @@ import "./Countstuff.css";
 const CountStuff = () => {
   return (
     <div class="main-count-stuff">
-      <div class="count-assets">
-        <img src="https://cdn-icons-png.flaticon.com/512/103/103414.png"></img>
-        <p>
-          423
-          <br />
-          <span>Assets</span>
-        </p>
-      </div>
       <div class="count-code-lines">
         <img src="https://cdn-icons-png.flaticon.com/512/6528/6528597.png"></img>
         <p>
-          423
+          496890
           <br />
           <span>Code Lines</span>
         </p>
@@ -24,7 +16,7 @@ const CountStuff = () => {
       <div class="count-work-hours">
         <img src="https://cdn-icons-png.flaticon.com/512/109/109613.png"></img>
         <p>
-          423
+          3625
           <br />
           <span>Work Hours</span>
         </p>
@@ -32,9 +24,17 @@ const CountStuff = () => {
       <div class="count-coffees">
         <img src="https://cdn-icons-png.flaticon.com/512/3127/3127336.png"></img>
         <p>
-          423
+          510
           <br />
           <span>Coffees</span>
+        </p>
+      </div>
+      <div class="count-serie-hours">
+        <img src="https://cdn-icons-png.flaticon.com/512/2790/2790961.png"></img>
+        <p>
+          363
+          <br />
+          <span>Series Days</span>
         </p>
       </div>
     </div>
