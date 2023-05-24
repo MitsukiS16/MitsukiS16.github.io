a1e16c226ae49318aaa95557a8da0b17f69871e5
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-05-24 18:19:14 +0100

    create portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,38 +4,74 @@ import "./Portfolio.css";
 
 const Portfolio = () => {
   const counts = [
-    { date: "2021", title: "project 1", link: "link1", tags: "ola2" },
-    { date: "2021", title: "project 2", link: "link1", tags: "ola2" },
-    { date: "2022", title: "project 3", link: "link1", tags: "ola2" },
-    { date: "2022", title: "project 4", link: "link1", tags: "ola2" },
-    { date: "2023", title: "project 5", link: "link1", tags: "ola2" },
+    {
+      date: "2021",
+      title: "Web scraping - Yugioh",
+      link: "https://github.com/MitsukiS16",
+      tags: ["python"],
+    },
+    {
+      date: "2022",
+      title: "Website clube judo 82",
+      link: "https://github.com/MitsukiS16",
+      tags: ["javaScript", "css", "html"],
+    },
+    {
+      date: "2022",
+      title: "Game borberman",
+      link: "https://github.com/MitsukiS16",
+      tags: ["java", "gradle"],
+    },
+    {
+      date: "2022",
+      title: "App S.U.P - Calculator",
+      link: "https://github.com/MitsukiS16",
+      tags: ["flutter", "dart", "html", "ruby"],
+    },
+    {
+      date: "2023",
+      title: "game questionnarie",
+      link: "https://github.com/MitsukiS16",
+      tags: ["c#", "ShaderLab"],
+    },
+    {
+      date: "2023",
+      title: "Website ticket system",
+      link: "https://github.com/MitsukiS16",
+      tags: ["php", "css", "javaScript"],
+    },
   ];
+  const openLink = (link) => {
+    window.open(link, "_blank");
+  };
 
   return (
     <section class="about portfolio">
       <h2>Portfolio</h2>
       <div class="main-portfolio">
         {counts.map((item) => (
-          <div class="project">
-            <h5>
-              <span>{item.date}</span>
-              {item.title}
-            </h5>
+          <div
+            class="specific-project"
+            key={item.title}
+            onClick={() => openLink(item.link)}
+          >
+            <div class="specific-project-main">
+              <div class="specific-project-date">
+                <p>{item.date}</p>
+              </div>
+              <div class="specific-project-title">
+                <h5>{item.title}</h5>
+              </div>
+            </div>
+            <div className="specific-project-tags">
+              {item.tags.map((tag, index) => (
+                <p className="tag" key={index}>
+                  {tag}
+                </p>
+              ))}
+            </div>
           </div>
         ))}
-
-        {/* <div class="project">
-          <p>project 2</p>
-        </div>
-        <div class="project">
-          <p>project 3</p>
-        </div>
-        <div class="project">
-          <p>project 4</p>
-        </div>
-        <div class="project">
-          <p>project 5</p>
-        </div> */}
       </div>
     </section>
   );
