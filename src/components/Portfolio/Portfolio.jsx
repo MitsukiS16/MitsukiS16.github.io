904c28508df6c68d8ef55e252f854f6c0df91769
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-05-24 09:00:29 +0100

    create var colors css

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,14 +3,28 @@ import React from "react";
 import "./Portfolio.css";
 
 const Portfolio = () => {
+  const counts = [
+    { date: "2021", title: "project 1", link: "link1", tags: "ola2" },
+    { date: "2021", title: "project 2", link: "link1", tags: "ola2" },
+    { date: "2022", title: "project 3", link: "link1", tags: "ola2" },
+    { date: "2022", title: "project 4", link: "link1", tags: "ola2" },
+    { date: "2023", title: "project 5", link: "link1", tags: "ola2" },
+  ];
+
   return (
     <section class="about portfolio">
       <h2>Portfolio</h2>
       <div class="main-portfolio">
-        <div class="project">
-          <p>project 1</p>
-        </div>
-        <div class="project">
+        {counts.map((item) => (
+          <div class="project">
+            <h5>
+              <span>{item.date}</span>
+              {item.title}
+            </h5>
+          </div>
+        ))}
+
+        {/* <div class="project">
           <p>project 2</p>
         </div>
         <div class="project">
@@ -21,7 +35,7 @@ const Portfolio = () => {
         </div>
         <div class="project">
           <p>project 5</p>
-        </div>
+        </div> */}
       </div>
     </section>
   );
