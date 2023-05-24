1b28089b69b55d4635aeb7c93f67f08769f52c84
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-05-24 20:57:04 +0100

    update my life stuff

--- src/components/MyLife/MyLife.jsx
+++ src/components/MyLife/MyLife.jsx
@@ -9,16 +9,16 @@ const MyLife = () => {
       <div className="main-my-life">
         <div className="specific-stuff">
           <div className="div-stuff div-stuff-1">
-            <h5>ola</h5>
+            <h5>Statics</h5>
           </div>
           <div className="div-stuff div-stuff-2">
-            <h5>ola</h5>
+            <h5>Travel</h5>
           </div>
           <div className="div-stuff div-stuff-3">
-            <h5>ola</h5>
+            <h5>Blog</h5>
           </div>
           <div className="div-stuff div-stuff-4">
-            <h5>ola</h5>
+            <h5>Cv</h5>
           </div>
         </div>
       </div>
