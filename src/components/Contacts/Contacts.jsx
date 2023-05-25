a9d7d3ed5f834b8b7499499b40a6d49ebec2fb53
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-05-25 15:23:11 +0100

    create section contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -3,9 +3,25 @@ import "./Contacts.css";
 const Contacts = () => {
   return (
     <section className="about contacts">
-      <h2>Contacts</h2>
+      <h2>Find me</h2>
       <div className="main-contacts">
-        <p>olaa</p>
+        <p>Lets create something together.</p>
+        <h5>
+          <span>Email</span> clarissemtcarvalho@gmail.com
+        </h5>
+        <br />
+        <h5>
+          <span>Phone</span>(+351) 917 090 276
+        </h5>
+        <h5>
+          <span>Instagram</span>@sissi_maria01
+        </h5>
+        <h5>
+          <span>LinkedIn</span>/clarissemaria
+        </h5>
+        <h5>
+          <span>Address</span>Porto, Portugal
+        </h5>
       </div>
     </section>
   );
