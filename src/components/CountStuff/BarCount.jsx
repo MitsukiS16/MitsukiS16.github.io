f6d847ff4738ded0df6b933a2b907e96a6572c39
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-07-10 17:08:42 +0100

    add count scroll number bar count

--- src/components/CountStuff/BarCount.jsx
+++ src/components/CountStuff/BarCount.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useEffect, useState, useRef } from "react";
 import "./BarCount.css";
 
 const BarCount = () => {
@@ -26,13 +25,85 @@ const BarCount = () => {
     },
   ];
 
+  const [animatedCounts, setAnimatedCounts] = useState([]);
+  const duration = 3000; // Total duration for the animation in milliseconds
+  const containerRef = useRef(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleScroll = () => {
+      if (container) {
+        const rect = container.getBoundingClientRect();
+        const isInView = rect.top >= 0 && rect.bottom <= window.innerHeight;
+        setIsInView(isInView);
+      }
+    };
+
+    const handleMouseEnter = () => {
+      setIsInView(true);
+    };
+
+    const handleMouseLeave = () => {
+      setIsInView(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    container.addEventListener("mouseenter", handleMouseEnter);
+    container.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      container.removeEventListener("mouseenter", handleMouseEnter);
+      container.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isInView) {
+      setAnimatedCounts(
+        counts.map((item) => ({
+          ...item,
+          animatedCount: 0,
+        }))
+      );
+    } else {
+      let startTime = null;
+
+      const animate = (timestamp) => {
+        if (!startTime) {
+          startTime = timestamp;
+        }
+
+        const elapsed = timestamp - startTime;
+
+        setAnimatedCounts((prevCounts) =>
+          prevCounts.map((item) => {
+            const increment =
+              (item.count / duration) * Math.min(elapsed, duration);
+            const animatedCount =
+              increment <= item.count ? Math.ceil(increment) : item.count;
+            return { ...item, animatedCount };
+          })
+        );
+
+        if (elapsed < duration) {
+          requestAnimationFrame(animate);
+        }
+      };
+
+      requestAnimationFrame(animate);
+    }
+  }, [isInView]);
+
   return (
-    <div className="main-count-stuff">
-      {counts.map((item) => (
-        <div className="count-stuff">
-          <img src={item.img}></img>
+    <div className="main-count-stuff" ref={containerRef}>
+      {animatedCounts.map((item, index) => (
+        <div className="count-stuff" key={index}>
+          <img src={item.img} alt={item.title} />
           <p>
-            {item.count}
+            {item.animatedCount}
             <br />
             <span>{item.title}</span>
           </p>
