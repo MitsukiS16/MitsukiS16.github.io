b6d1a557cf8b85159d68186bd92d2b583ec42c13
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-07-11 11:37:04 +0100

    fix underline words about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -20,12 +20,12 @@ const AboutMe = () => {
           </p>
           <br />
           <p>
-            As a UI Designer and FullStack Developer, I find great joy in
-            creating code that is not only elegant but also{" "}
+            As a UI Designer and <span>FullStack</span> Developer, I find great
+            joy in creating code that is not only <span>elegant</span> but also{" "}
             <span>efficient</span>. It brings me a sense of accomplishment and
             satisfaction when I can produce clean and well-designed solutions
-            that not only look visually appealing but also provide seamless
-            functionality.
+            that not only look visually appealing but also provide{" "}
+            <span>seamless</span> functionality.
           </p>
           <br />
           <p>
